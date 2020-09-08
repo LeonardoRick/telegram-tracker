533548eb9fb8832d150cbf36cbe5398125c23c04
Leonardo Rick <leonardo@lanytecnologia.com.br>
2020-09-08 20:30:44 -0300

    finish setup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # telegram-tracker
 Basic telegram tracker using python-telegram library that read all messages sent to logged user and save on a file
+
+# Startup
+1) First, config your own telegram API and writedown your API_HASH and API_ID:
+https://my.telegram.org/auth
+
+2) on tlg.py update those infos (API_HASH and API_ID) on it's fields over Telegram Object
+
+3) run python tlg.py. Your log will be saved on log.txt
+
---
+++ tlg.py
@@ -0,0 +1,33 @@
+import sys
+from telegram.client import Telegram
+from iqlogger import Logger
+from pathlib import Path
+import tlg_constants
+
+# CLASS just to test telegram
+def tlgMsgHandler(update):
+    # we want to process only text messages
+
+    message_content = update['message']['content'].get('text', {})
+    msg = message_content.get('text', '')
+    chat_id = str(update['message']['chat_id'])
+    print(f'chat Id: : {chat_id}\nmessage: {msg}')
+
+
+constants = tlg_constants.TelegramConstants()
+
+tlg: Telegram = Telegram(
+    api_id=constants.api_id,
+    api_hash=constants.api_hash,
+    phone=constants.phone,
+    database_encryption_key=constants.api_hash
+)
+
+sys.stdout = Logger(Path(__file__).parent.absolute())
+
+tlg.login()  # connect to Telegram
+tlg.add_message_handler(tlgMsgHandler)
+
+
+while True:
+    pass
